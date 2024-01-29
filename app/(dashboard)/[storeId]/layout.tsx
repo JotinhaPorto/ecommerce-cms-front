84038e436256f9a866ba8481657d3c9195a3a788
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2024-01-29 17:03:47 -0300

    Created navbar  and added them to layout dashboard page

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/navbar'
 import React from 'react'
 type Props = { params: { storeId: string }, children: React.ReactNode }
 const page = ({ params, children }: Props) => {
     return (
-        <div>page of STORE - {params.storeId}
+        <div>
+            <Navbar />
             {children}
         </div>
     )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,15 +6,14 @@ import { Store } from '@/app/types/Store'
 import MainNav from './mainnav'
 import UserNav from './user-nav'
 import { getCurrentUser } from '@/actions/getCurrentUser'
+
 const Navbar = async () => {
 
     const token = cookies().get("token")
 
     const stores: Store[] = await getAllStores(token?.value as string)
-
     const user = await getCurrentUser()
 
-    console.log(user)
 
     return (
         <div className='border-b shadow-md px-4 h-14 flex items-center'>
