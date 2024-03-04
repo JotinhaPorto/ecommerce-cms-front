129d27c944c91c290a8d3bd8e87eb74d95914740
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2024-03-04 16:28:10 -0300

    Development pages

--- app/(dashboard)/[storeId]/pedidos/page.tsx
+++ app/(dashboard)/[storeId]/pedidos/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button"
-import { MoveRight, Trash } from "lucide-react"
-import Image from "next/image"
+import React from 'react'
 
 const page = () => {
     return (
